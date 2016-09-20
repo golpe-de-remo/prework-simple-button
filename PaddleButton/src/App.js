7759ee62c565ea86bb285795117f96c12d030bac
Lucatis <llucmillat@gmail.com>
2016-09-20 12:39:19 +0200

    Humble contribution with almost no cheating

---
+++ PaddleButton/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="wrapper">
+          <button>Paddle!</button>
+        </div>
+
+      
+
+    );
+  }
+}
+
+
+export default App;
