6aae0bfd7acefe58a6dbbfb4d69e39e50f536ecd
Juan Zerbini Berro <juanze82@hotmail.com>
2016-09-21 22:31:32 +0200

    My button again

---
+++ simple-button/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+	<div>
+      <button>Example</button>
+	</div>
+    );
+  }
+}
+export default App;
